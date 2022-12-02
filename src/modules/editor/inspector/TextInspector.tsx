661443f8b9d300a42006f04f4422eddb2b6b1fc6
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2022-12-02 15:42:10 +0530

    feat: added ui for text formatting

--- src/modules/editor/inspector/TextInspector.tsx
+++ src/modules/editor/inspector/TextInspector.tsx
@@ -4,6 +4,8 @@ import {
   Align,
   Node,
   TextboxNode,
+  TextNode,
+  TextStyles,
   VerticalAlign,
 } from "modules/state/project/ProjectTypes";
 import {
@@ -42,9 +44,9 @@ export const TextInspector = ({ node }: TextInspectorProps) => {
     { name: "underline", icon: Underline },
   ];
   const styles = [
-    { name: "heading-1", icon: Type },
-    { name: "heading-2", icon: Type },
-    { name: "heading-3", icon: Type },
+    { name: "heading-1", icon: Type, size: 18 },
+    { name: "heading-2", icon: Type, size: 16 },
+    { name: "heading-3", icon: Type, size: 14 },
   ];
   return (
     <div>
@@ -92,11 +94,29 @@ export const TextInspector = ({ node }: TextInspectorProps) => {
             })}
             onClick={() => {
               AppStore.project.setNode(node.id, {
-                [style.name]: true,
+                [style.name]: !(node as any)[style.name],
               });
             }}
           >
             <style.icon size={15} />
+          </div>
+        ))}
+      </div>
+      <div className="text-sm my-2">Styles</div>
+      <div className="flex flex-row gap-3">
+        {styles.map((style) => (
+          <div
+            key={style.name}
+            className={clsx("cursor-pointer p-2", {
+              "bg-slate-300": (node as TextNode).style === style.name,
+            })}
+            onClick={() => {
+              AppStore.project.setNode(node.id, {
+                style: style.name as TextStyles,
+              });
+            }}
+          >
+            <style.icon size={style.size} />
           </div>
         ))}
       </div>
