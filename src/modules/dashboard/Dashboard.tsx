a303c682ee08bd6053b63b9d2e2dc06b3f43f3e7
Vikram Thyagarajan <vikramthyagarajan92@gmail.com>
2023-04-30 18:25:57 +0530

    feat: showing some random images for dashboard thumbnails

--- src/modules/dashboard/Dashboard.tsx
+++ src/modules/dashboard/Dashboard.tsx
@@ -22,6 +22,15 @@ interface BoardProps {
   board: Board;
 }
 
+const images = [
+  "https://res.cloudinary.com/diglgjher/image/upload/v1682674505/boards/25065f8d-d971-48b0-b1c5-0333d97f0154.png",
+  "https://res.cloudinary.com/diglgjher/image/upload/v1682674497/boards/8c653bf5-9789-46cc-b10d-4adb34296517.png",
+  "https://res.cloudinary.com/diglgjher/image/upload/v1682675049/boards/b2ccbfc9-a354-465f-a574-cd7fbdd2de84.png",
+  "https://res.cloudinary.com/diglgjher/image/upload/v1682675043/boards/52069cc5-ff79-49c3-b60e-8d422d0edade.png",
+  "https://res.cloudinary.com/diglgjher/image/upload/v1682587980/boards/9ad8a850-48bf-4dd0-93b9-fef6d25a4a74.png",
+  "https://res.cloudinary.com/diglgjher/image/upload/v1682675043/boards/52069cc5-ff79-49c3-b60e-8d422d0edade.png",
+];
+
 const generateFakeData = (): Board[] => {
   const images = [
     "https://res.cloudinary.com/diglgjher/image/upload/v1682674505/boards/25065f8d-d971-48b0-b1c5-0333d97f0154.png",
@@ -38,9 +47,11 @@ const generateFakeData = (): Board[] => {
 };
 
 const Board = ({ board }: BoardProps) => {
+  const random = Math.round(Math.random() * 5);
+  const url = images[random];
   return (
     <div className="rounded-md border border-slate-400 w-[200px] cursor-pointer overflow-hidden shadow-sm">
-      <img src={board.image} className="w-[200px] h-[200px]"></img>
+      <img src={url} className="w-[200px] h-[200px]"></img>
       <div className="flex bg-white px-2 py-4 rounded-b-md items-start gap-1">
         <Edit2 width={20} height={20} className="mt-1 w-5 h-5"></Edit2>
         <div className="text-sm capitalize text-ellipsis font-[200] flex-1">
